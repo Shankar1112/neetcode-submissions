17fc19ed6612fd96d2f2f9876adc906b39940694
Shankar Manickavasagam <smanicka@asu.edu>
2026-05-12 23:57:23 -0700

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int rows = m;
+        int cols = n;
+        int[][] dp = new int[rows][cols];
+
+        for (int i = 0; i < cols; i++) {
+            dp[0][i] = 1;
+        }
+
+         for (int i = 0; i < rows; i++) {
+            dp[i][0] = 1;
+        } 
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[rows-1][cols-1];        
+    }
+}
