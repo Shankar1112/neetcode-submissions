6b735ff9a67dfb8913e22facc0d73b0b435eeaed
Shankar Manickavasagam <smanicka@asu.edu>
2026-05-27 23:47:12 -0700

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int stone: stones) {
+            pq.offer(stone);
+        }
+
+        while (pq.size() > 1) {
+            int x = pq.poll();
+            int y = pq.poll();
+            pq.offer(x - y);
+        }
+        return pq.peek();
+    }
+}
