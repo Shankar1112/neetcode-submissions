76c15002986524cdd59ee728ea592b98d067d7cd
Shankar Manickavasagam <smanicka@asu.edu>
2026-05-28 23:19:27 -0700

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.java
@@ -0,0 +1,51 @@
+class DSU {
+    int[] parent;
+    int[] rank;
+
+    public DSU (int n) {
+        parent = new int[n];
+        rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public boolean union( int x, int y) {
+        int px = find(x), py = find(y);
+
+        if (px == py) {
+            return false;
+        }
+
+        if (rank[px] > rank[py]) {
+            parent[py] = px;
+        } else if (rank[px] < rank[py]) {
+            parent[px] = py;
+        } else {
+            parent[py] = px;
+            rank[px]++;
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int connected = n;
+        DSU dsu = new DSU(n);
+        for (int[] edge: edges) {
+            if (dsu.union(edge[0], edge[1])) {
+                connected--;
+            }
+        }
+        return connected;
+    }
+}
