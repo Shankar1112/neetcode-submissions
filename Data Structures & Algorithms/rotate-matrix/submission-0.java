860515ed4077bf2c2cb72ab12b250442014ce80b
Shankar Manickavasagam <smanicka@asu.edu>
2026-05-30 20:01:53 -0700

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        swap(matrix);
+    }
+
+    void transpose(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i+1; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    void swap(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            int left = 0, right = matrix.length - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
